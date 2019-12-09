2369d46474cb779518f64a50460b36c2bf698aca
yingpu <1754250450@qq.com>
2019-12-09 17:16:47 +0800

    提交

---
+++ C#/01/MVC/MVCTest/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using MVCTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTest.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            Customer customer = new Customer();
+            customer.id = id;
+            customer.strName = "赵大";
+            ViewData.Model = customer;
+            return View();
+        }
+
+    }
+}
---
+++ C#/01/MVC/MVCTest/Controllers/UserInfoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTest.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        //
+        // GET: /UserInfo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterInfo()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterResult()
+        {
+            string str = Request["txtname"];
+            return Content(str);
+        }
+    }
+}
