06c76c88d1ce609e0f60dc9557ce1061775f2c1f
yingpu <1754250450@qq.com>
2019-12-10 14:54:52 +0800

    EF For Orcale

---
+++ C#/01/MVC/EFTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Entities shiny = new Entities();
+            var lscamera = shiny.PT_CAMERA_INFO.ToList();
+            Console.WriteLine("PT_CameraInfo表"+lscamera.Count()+"条数据");
+            Console.ReadKey();
+            shiny.SaveChanges();
+        }
+    }
+}
